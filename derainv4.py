7029a749430542cffb503968a5d8e445e8de350b
Kushal P <f2014624@goa.bits-pilani.ac.in>
2017-12-15 21:39:00 +0530

    Add files via upload

--- derainv4.py
+++ derainv4.py
@@ -18,7 +18,8 @@ def psnr(base_image,altered_image):
     if(MSE==0):
        return 100
     else:
-       return 20*K.log(255.0/K.sqrt(MSE))
+       return (20*K.log(255.0/K.sqrt(MSE)))/K.log(10.0)
+
 
 class Derain(object):
 
@@ -65,7 +66,18 @@ class Derain(object):
             self.ori[i] = cv2.resize(self.ori[i], (self.y, self.x))
             self.rainy[i] = cv2.resize(self.rainy[i], (self.y, self.x))
 
-
+    def loadmodel(self):
+         #model = Sequential()
+    	 file_name = "weights.40-678.80.hdf5"
+    	 #model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy',psnr])
+         model1=keras.models.load_model(file_name,custom_objects = {'psnr':psnr});
+         d={'rainy':np.asarray(self.rainy),'original':np.asarray(self.ori)}
+         print model1.evaluate(d['rainy'].reshape(-1,256,256,3),d['original'].reshape(-1,256,256,3),batch_size = 1)
+         #model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy',psnr])
+         for i in range(20):
+            di=model1.predict(np.reshape(self.rainy[i],[1,256,256,3]))
+            dd=np.reshape(di,[256,256,3])
+            cv2.imwrite("./outputs/"+str(i)+"predict.jpg",dd)
 
 
 
@@ -162,8 +174,7 @@ class Derain(object):
 
         model.add(Conv2DTranspose(3, filtersize, strides=(1,1),padding='same',data_format="channels_last",activation='relu'))
 
-        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy',psnr])
-
+        model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
         model.fit(d['rainy'],d['original'],  epochs=epochs, batch_size=batch_size,validation_split=0.3, callbacks=[self.mc])
 
@@ -173,11 +184,9 @@ class Derain(object):
         #ii=np.asarray(ii)
         #ii=ii[:512. :512, :]
         #ii=cv2.resize(ii,(512,512))
-        di=model.predict(np.reshape(self.rainy[2],[1,256,256,3]))
-        cv2.imshow('', np.reshape(di,[256,256,3]))
-        cv2.waitKey(0)
-        cv2.imshow('', np.reshape(self.rainy[2],[256,256,3]))
-        cv2.waitKey(0)
+        
+        
+        
         # plt.imshow(np.reshape(self.rainy[0],[512,512,3]))
 
         '''plt.imshow(self.ori[0])
@@ -242,9 +251,9 @@ class Derain(object):
         deconv_2_decoded = decoder_deconv_2(deconv_1_decoded)
         x_decoded_relu = decoder_deconv_3_upsamp(deconv_2_decoded)
         x_decoded_mean_squash = decoder_mean_squash(x_decoded_relu)'''
-'''
-    def save_model(self, step):
-        # file_name = params['name']
+
+    def save_model(self, step):    #train function saves the model too
+        
         # pickle.dump(self, gzip.open(file_name, 'wb'))
 
         """
@@ -254,192 +263,12 @@ class Derain(object):
         """
 
 
-    def load_model(self, **params):
+
     	# file_name = params['name']
         # return pickle.load(gzip.open(file_name, 'rb'))
 
-        """
-            returns a pre-oried instance of Segment class
-        """
+        
+    
 
-    def graph(self):
-
-    	with tf.device('/gpu:1'):
-
-            	self.x = tf.placeholder(tf.float32, shape=(1,None, None, 3))
-	    	self.flat_labels = tf.placeholder(tf.int64, shape=(1,None,2))
-	    	labels = tf.reshape(self.flat_labels, (-1, 2))
-
-	    	#conv layer 1
-	    	W_shape = [2, 2, 3, 64]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_1 =  tf.nn.relu(tf.nn.conv2d(self.x, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-		#conv layer 2
-	    	W_shape = [3, 3, 64, 64]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_2 =  tf.nn.relu(tf.nn.conv2d(conv_1, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-		# pool layer 1
-		pool_1, pool_1_argmax = tf.nn.max_pool_with_argmax(conv_2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-		#conv layer 3
-	    	W_shape = [3, 3, 64, 128]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_3 =  tf.nn.relu(tf.nn.conv2d(pool_1, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-	    	#conv layer 4
-	    	W_shape = [3, 3, 128, 128]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_4 =  tf.nn.relu(tf.nn.conv2d(conv_3, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-
-		# pool layer 2
-		pool_2, pool_2_argmax = tf.nn.max_pool_with_argmax(conv_4, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-
-
-	    	#conv layer 5
-	    	W_shape = [3, 3, 128, 256]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_5 =  tf.nn.relu(tf.nn.conv2d(pool_2, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-		#conv layer 6
-	    	W_shape = [3, 3, 256, 256]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-		b = tf.Variable(tf.constant(0.1, shape=[W_shape[3]]))
-		conv_6 =  tf.nn.relu(tf.nn.conv2d(conv_5, W, strides=[1, 1, 1, 1], padding='SAME') + b)
-
-		pool_3, pool_3_argmax = tf.nn.max_pool_with_argmax(conv_6, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
-		dropout = tf.nn.dropout(pool_3, keep_prob = 0.5)
-
-		#unpool 3
-		padding = 'SAME'
-		unpool_3 = self.unpool(dropout, pool_3_argmax, tf.shape(conv_6))
-
-
-		    #deconv layer 6
-	    	W_shape = [3, 3, 256, 256]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = unpool_3
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-	        deconv_6 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-
-            	#deconv layer 5
-	    	W_shape = [3, 3, 128, 256]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = deconv_6
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	deconv_5 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-            	unpool_2 = self.unpool(deconv_5, pool_2_argmax, tf.shape(conv_4))
-
-            	#deconv layer 4
-	    	W_shape = [3, 3, 128, 128]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = unpool_2
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	deconv_4 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-
-            	#deconv layer 3
-	    	W_shape = [3, 3, 64, 128]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = deconv_4
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	deconv_3 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-            	unpool_1 = self.unpool(deconv_3, pool_1_argmax, tf.shape(conv_2))
-
-            	#deconv layer 2
-            	W_shape = [3, 3, 64, 64]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = unpool_1
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	deconv_2 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-            	#deconv layer 1
-            	W_shape = [3, 3, 32, 64]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = deconv_2
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	deconv_1 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-
-            	#deconv layer 0
-            	W_shape = [1, 1, 2, 32]
-	    	W = tf.Variable(tf.truncated_normal(W_shape, stddev=0.1))
-	        b = tf.Variable(tf.constant(0.1, shape=[W_shape[2]]))
-	        x = deconv_1
-	        x_shape = tf.shape(x)
-        	out_shape = tf.pack([x_shape[0], x_shape[1], x_shape[2], W_shape[2]])
-        	score_1 = tf.nn.conv2d_transpose(x, W, out_shape, [1, 1, 1, 1], padding=padding) + b
-
-            	flat_logits = tf.reshape(score_1, (-1, 2))
-            	cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits = flat_logits, labels = labels, name='x_entropy')
-            	self.loss = tf.reduce_mean(cross_entropy, name='x_entropy_mean')
-
-            	self.train_step = tf.train.AdamOptimizer(1e-3).minimize(self.loss)
-
-            	self.prediction = tf.argmax(flat_logits,1)
-
-
-    def unpool(self, x, argmax, out_shape):
-	shape = tf.to_int64(out_shape)
-        argmax = tf.pack([argmax // (shape[2] * shape[3]), argmax % (shape[2] * shape[3]) // shape[3]])
-        output = tf.zeros([out_shape[1], out_shape[2], out_shape[3]])
-        height = tf.shape(output)[0]
-        width = tf.shape(output)[1]
-        channels = tf.shape(output)[2]
-        indices1 = tf.squeeze(argmax)
-        indices1 = tf.transpose(tf.pack((indices1[0], indices1[1]), axis=0), perm=[3, 1, 2, 0])
-        indices2 = tf.tile(tf.to_int64(tf.range(channels)), [((width + 1) // 2) * ((height + 1) // 2)])
-        indices2 = tf.transpose(tf.reshape(indices2, [-1, channels]), perm=[1, 0])
-        indices2 = tf.reshape(indices2, [channels, (height + 1) // 2, (width + 1) // 2, 1])
-        indices = tf.reshape(tf.concat(3,  [indices1, indices2]), [((height + 1) // 2) * ((width + 1) // 2) * channels, 3])
-        inter = tf.reshape(tf.squeeze(x), [-1, channels])
-        values = tf.reshape(tf.transpose(inter, perm=[1, 0]), [-1])
-        delta = tf.SparseTensor(indices, values, tf.to_int64(tf.shape(output)))
-        return tf.expand_dims(tf.sparse_tensor_to_dense(tf.sparse_reorder(delta)), 0)
-
-
-if __name__ == "__main__":
-
-        obj = Derain('./Training')
-	# training the model
-	X = range(0,164)
-        k_fold = KFold(n_splits=5)
-	i = 0
-	for train_indices, test_indices in k_fold.split(X):
-		print( '{} validation'.format(i))
-		i += 1
-    		print('Train: %s | test: %s' % (train_indices, test_indices))
-		obj.train(train_indices)
-		print 'Accuracy = {:.3f}%'.format(obj.test(test_indices) * 100)
-
-       	# testing the model
-	T = range(0,5)
-	print( 'Accuracy = {:.3f}%'.format(obj.test(T) * 100))
-
-
-'''
-p = Derain("./training", "./checkpoints")
-p.train()
+p = Derain("./test", "./checkpoints")
+p.loadmodel()
